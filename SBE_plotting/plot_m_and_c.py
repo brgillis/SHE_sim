4eabdaa055271662edef48566701f59d506e2fe8
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2015-08-13 11:52:47 +0100

    Adding FDNT compatibility

--- SBE_plotting/plot_m_and_c.py
+++ SBE_plotting/plot_m_and_c.py
@@ -18,23 +18,27 @@ matplotlib.rcParams['text.usetex'] = True
 
 def main(argv):
     
-    team_names = ("Control", "1", "2", "3")
+    team_names = ("Control (KSB)", "1", "2", "3", "4")
     
     low_SN_data_files = ("/home/brg/git/SHE_sim/Generate_GalSim_Images/trunk/benchmark_low_SN/stored_data.fits",
                           "/home/brg/Data/SBE/MegaLUT/low_SN/stored_data.fits",
                           "/home/brg/Data/SBE/Oxford/low_SN/stored_data.fits",
-                          "/home/brg/Data/SBE/gfit/high_SN/stored_data.fits")
+                          "/home/brg/Data/SBE/gfit/low_SN/stored_data.fits",
+                          "/home/brg/Data/SBE/FDNT/low_SN/stored_data.fits")
     high_SN_data_files = ("/home/brg/git/SHE_sim/Generate_GalSim_Images/trunk/benchmark_high_SN/stored_data.fits",
                           "/home/brg/Data/SBE/MegaLUT/high_SN/stored_data.fits",
                           "/home/brg/Data/SBE/Oxford/high_SN/stored_data.fits",
-                          "/home/brg/Data/SBE/gfit/high_SN/stored_data.fits")
+                          "/home/brg/Data/SBE/gfit/high_SN/stored_data.fits",
+                          "/home/brg/Data/SBE/FDNT/high_SN/stored_data.fits")
     
     num_teams = len(team_names)
     m_limits = (0.001,0.3)
     c_limits = (0.5e-5,1.5e-3)
+    m_err_limits = (0,0.01)
+    c_err_limits = (0,1e-4)
     
     m_target = 2e-3
-    c_target = 1e-5
+    c_target = 5e-4
     
     fontsize = 24
     SN_shift = 0.05
@@ -137,6 +141,25 @@ def main(argv):
     high_SN_c_stderr_pix = 0.5*np.sqrt(np.square(high_SN_c1_stderr_pix)+np.square(high_SN_c2_stderr_pix))
     high_SN_c_stderr_psf = 0.5*np.sqrt(np.square(high_SN_c1_stderr_psf)+np.square(high_SN_c2_stderr_psf))
     
+    # Get calibration errors
+    low_SN_m_cal_err_pix = 0.5*np.sqrt(np.square(low_SN_m1_stderr_pix/np.add(1,low_SN_m1_pix))+
+                                       np.square(low_SN_m2_stderr_pix/np.add(1,low_SN_m2_pix)))
+    low_SN_m_cal_err_psf = 0.5*np.sqrt(np.square(low_SN_m1_stderr_psf/np.add(1,low_SN_m1_psf))+
+                                       np.square(low_SN_m2_stderr_psf/np.add(1,low_SN_m2_psf)))
+    low_SN_c_cal_err_pix = 0.5*np.sqrt(np.square(low_SN_c1_stderr_pix/np.add(1,low_SN_m1_pix))+
+                                       np.square(low_SN_c2_stderr_pix/np.add(1,low_SN_m2_pix)))
+    low_SN_c_cal_err_psf = 0.5*np.sqrt(np.square(low_SN_c1_stderr_psf/np.add(1,low_SN_m1_psf))+
+                                       np.square(low_SN_c2_stderr_psf/np.add(1,low_SN_m2_psf)))
+    
+    high_SN_m_cal_err_pix = 0.5*np.sqrt(np.square(high_SN_m1_stderr_pix/np.add(1,high_SN_m1_pix))+
+                                        np.square(high_SN_m2_stderr_pix/np.add(1,high_SN_m2_pix)))
+    high_SN_m_cal_err_psf = 0.5*np.sqrt(np.square(high_SN_m1_stderr_psf/np.add(1,high_SN_m1_psf))+
+                                        np.square(high_SN_m2_stderr_psf/np.add(1,high_SN_m2_psf)))
+    high_SN_c_cal_err_pix = 0.5*np.sqrt(np.square(high_SN_c1_stderr_pix/np.add(1,high_SN_m1_pix))+
+                                        np.square(high_SN_c2_stderr_pix/np.add(1,high_SN_m2_pix)))
+    high_SN_c_cal_err_psf = 0.5*np.sqrt(np.square(high_SN_c1_stderr_psf/np.add(1,high_SN_m1_psf))+
+                                        np.square(high_SN_c2_stderr_psf/np.add(1,high_SN_m2_psf)))
+    
     # Set up indices for x-axis
     team_indices = np.array(range(num_teams))
     
@@ -190,10 +213,10 @@ def main(argv):
     
     # Plot errors
     for (low_SN_y_stderr_array, high_SN_y_stderr_array, y_limits, filename, y_label) in \
-                    ((low_SN_m_stderr_pix,high_SN_m_stderr_pix,m_limits,"SBE_m_err_pix.eps",r"$\sigma(|m|)$ in pixel frame"),
-                     (low_SN_m_stderr_psf,high_SN_m_stderr_psf,m_limits,"SBE_m_err_psf.eps",r"$\sigma(|m|)$ in psf frame"),
-                     (low_SN_c_stderr_pix,high_SN_c_stderr_pix,c_limits,"SBE_c_err_pix.eps",r"$\sigma(|c|)$ in pixel frame"),
-                     (low_SN_c_stderr_psf,high_SN_c_stderr_psf,c_limits,"SBE_c_err_psf.eps",r"$\sigma(|c|)$ in psf frame")):
+                    ((low_SN_m_cal_err_pix,high_SN_m_cal_err_pix,m_err_limits,"SBE_m_err_pix.eps",r"$\sigma(|m|)/(1+m)$ in pixel frame"),
+                     (low_SN_m_cal_err_psf,high_SN_m_cal_err_psf,m_err_limits,"SBE_m_err_psf.eps",r"$\sigma(|m|)/(1+m)$ in psf frame"),
+                     (low_SN_c_cal_err_pix,high_SN_c_cal_err_pix,c_err_limits,"SBE_c_err_pix.eps",r"$\sigma(|c|)/(1+m)$ in pixel frame"),
+                     (low_SN_c_cal_err_psf,high_SN_c_cal_err_psf,c_err_limits,"SBE_c_err_psf.eps",r"$\sigma(|c|)/(1+m)$ in psf frame")):
         
         # Setup the figure
         fig = pyplot.figure()
@@ -210,7 +233,7 @@ def main(argv):
                     markersize=10,label="High S/N")
         
         # Draw the target line
-        if(y_limits==m_limits):
+        if(y_limits==m_err_limits):
             target = m_target
         else:
             target = c_target
@@ -224,7 +247,7 @@ def main(argv):
         ax.set_xticks(range(num_teams))
         ax.set_xticklabels(team_names,fontsize=fontsize)
         
-        ax.set_yscale("log", nonposy='clip')
+        #ax.set_yscale("log", nonposy='clip')
         ax.set_ylim(y_limits)
         ax.tick_params(axis='y', which='major', labelsize=fontsize)
         
@@ -233,6 +256,8 @@ def main(argv):
         pyplot.savefig(filename, format="eps", bbox_inches="tight", pad_inches=0.05)
         
         print("Finished plotting to " + filename + ".")
+        
+        return
     
 
 if __name__ == "__main__":
