cdd85a0a05df94444bc5d36fb8cf620df411e2ac
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2015-11-30 14:41:42 +0000

    Fixing filenames

--- SHE_SIM_gal_params/SHE_SIM_gal_params/params/independent_params.hpp
+++ SHE_SIM_gal_params/SHE_SIM_gal_params/params/independent_params.hpp
@@ -1,5 +1,5 @@
 /**********************************************************************\
- @file DEPENDENT_PARAMs.hpp
+ @file independent_params.hpp
  ------------------
 
  TODO <Insert file description here>
@@ -23,8 +23,8 @@
 
  \**********************************************************************/
 
-#ifndef SHE_SIM_GAL_PARAMS_PARAMS_DEPENDENT_PARAMS_HPP_
-#define SHE_SIM_GAL_PARAMS_PARAMS_DEPENDENT_PARAMS_HPP_
+#ifndef SHE_SIM_GAL_PARAMS_PARAMS_INDEPENDENT_PARAMS_HPP_
+#define SHE_SIM_GAL_PARAMS_PARAMS_INDEPENDENT_PARAMS_HPP_
 
 #include <SHE_SIM_gal_params/common.hpp>
 #include <SHE_SIM_gal_params/default_param_params.hpp>
@@ -39,27 +39,9 @@ namespace SHE_SIM
 
 // Define a macro for each param
 
-#define DEPENDENT_PARAM( class_name, param_name, dependent_generation ) \
+#define INDEPENDENT_PARAM(class_name,param_name) \
 class class_name : public ParamGenerator \
 { \
-private: \
-\
-	virtual void _generate() override \
-	{ \
-		if(_params->get_mode()==ParamParam::DEPENDENT) \
-		{ \
-			_cached_value = dependent_generation; \
-		} \
-		else if(_params->get_mode()==ParamParam::INDEPENDENT) \
-		{ \
-			_cached_value = _params->get_independently(_rng); \
-		} \
-		else \
-		{ \
-			throw bad_mode_error(_params->get_mode_name()); \
-		} \
-	} \
-\
 public: \
 	class_name( owner_t & owner) \
 	: ParamGenerator(owner) \
@@ -85,14 +67,38 @@ public: \
 // Define each param
 
 // Survey level
-DEPENDENT_PARAM(MagIZp,mag_i_zp,_request_param_value(mag_i_inst_zp_name)
-				                + 2.5* std::log10(_request_param_value(exp_time_name)));
-DEPENDENT_PARAM(MagVisZp,mag_vis_zp,_request_param_value(mag_vis_inst_zp_name)
-								+ 2.5* std::log10(_request_param_value(exp_time_name)));
+INDEPENDENT_PARAM(ExposureTime, exp_time);
+INDEPENDENT_PARAM(Gain, gain);
+INDEPENDENT_PARAM(MagIInstZp, mag_i_inst_zp);
+INDEPENDENT_PARAM(MagVisInstZp, mag_vis_inst_zp);
+INDEPENDENT_PARAM(PixelScale, pixel_scale);
+INDEPENDENT_PARAM(ReadNoise, read_noise);
+
+// Image level
+INDEPENDENT_PARAM(BackgroundGalaxyDensity, background_galaxy_density);
+INDEPENDENT_PARAM(ClusterDensity, cluster_density);
+INDEPENDENT_PARAM(FieldGalaxyDensity, field_galaxy_density);
+INDEPENDENT_PARAM(PSFParams, psf_params);
+INDEPENDENT_PARAM(StarDensity, star_density);
+INDEPENDENT_PARAM(SubtractedBackground, subtracted_background);
+INDEPENDENT_PARAM(UnsubtractedBackground, unsubtracted_background);
+INDEPENDENT_PARAM(ImageSizeXp, image_size_xp);
+INDEPENDENT_PARAM(ImageSizeYp, image_size_yp);
+
+// Cluster level
+INDEPENDENT_PARAM(ClusterMass, cluster_mass);
+INDEPENDENT_PARAM(ClusterRedshift, cluster_redshift);
+INDEPENDENT_PARAM(ClusterNumSatellites, cluster_num_satellites);
+INDEPENDENT_PARAM(ClusterXp, cluster_xp);
+INDEPENDENT_PARAM(ClusterYp, cluster_yp);
+
+// GalaxyDither level
+INDEPENDENT_PARAM(DitherXpShift, dither_xp_shift);
+INDEPENDENT_PARAM(DitherYpShift, dither_yp_shift);
 
 // Undef the macro
-#undef DEPENDENT_PARAM
+#undef INDEPENDENT_PARAM
 
 } // namespace SHE_SIM
 
-#endif // SHE_SIM_GAL_PARAMS_PARAMS_DEPENDENT_PARAMS_HPP_
+#endif // SHE_SIM_GAL_PARAMS_PARAMS_INDEPENDENT_PARAMS_HPP_
